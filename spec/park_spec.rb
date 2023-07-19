4c80ab3c6a97ac634be0a5a3f809999e25cb3a03
Eliza Keating <ek.elizakeating@gmail.com>
2023-07-19 10:06:52 -0600

    remove tracker class, add iteration 4 tests to park spec

--- spec/park_spec.rb
+++ spec/park_spec.rb
@@ -81,4 +81,66 @@ RSpec.describe Park do
       expect(park.revenue).to eq(10)
     end
   end
+
+  describe "#all_attendees" do
+    it "returns all attendees names, sorted alphabetically" do
+      park = Park.new("Windmill Park", 5)
+      vehicle = Vehicle.new("2001", "Honda", "Civic")
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+      jude = Passenger.new({"name" => "Jude", "age" => 20})    
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+      jill = Passenger.new({"name" => "Jill", "age" => 10})
+
+      vehicle.add_passenger(charlie)
+      vehicle.add_passenger(jude)
+      vehicle.add_passenger(taylor)
+      vehicle.add_passenger(jill)
+
+      park.add_vehicle(vehicle)
+
+      expect(park.all_attendees(park)).to eq(["Charlie", "Jill", "Jude", "Taylor"])
+    end
+  end
+
+  xdescribe "#minors" do
+    it "returns all minors names, sorted alphabetically" do
+      park = Park.new("Windmill Park", 5)
+      vehicle = Vehicle.new("2001", "Honda", "Civic")
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+      jude = Passenger.new({"name" => "Jude", "age" => 20})    
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+      jill = Passenger.new({"name" => "Jill", "age" => 10})
+
+      vehicle.add_passenger(charlie)
+      vehicle.add_passenger(jude)
+      vehicle.add_passenger(taylor)
+      vehicle.add_passenger(jill)
+
+      park.add_vehicle(vehicle)
+
+      expect(park.minors(park)).to eq(["Jill", "Taylor"])
+    end
+  end
+
+  xdescribe "#adults" do
+    it "returns all adults names, sorted alphabetically" do
+      park = Park.new("Windmill Park", 5)
+      vehicle = Vehicle.new("2001", "Honda", "Civic")
+      charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+      jude = Passenger.new({"name" => "Jude", "age" => 20})    
+      taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+      jill = Passenger.new({"name" => "Jill", "age" => 10})
+      eliza = Passenger.new({"name" => "Eliza", "age" => 21})
+
+      vehicle.add_passenger(charlie)
+      vehicle.add_passenger(jude)
+      vehicle.add_passenger(taylor)
+      vehicle.add_passenger(jill)
+      vehicle.add_passenger(eliza)
+
+      park.add_vehicle(vehicle)
+
+      expect(park.adults).to eq(["Charlie", "Eliza", "Jude"])
+    end
+  end
 end
